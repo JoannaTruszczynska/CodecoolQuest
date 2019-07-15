09b5ef71d735f2baa3500fcdf72aa439cf4df2c7
Szymon Seliga <szymon.seliga@codecool.com>
2019-07-15 09:27:24 +0200

    Code Cleanup

--- Codecool.Quest/MainWindow.xaml.cs
+++ Codecool.Quest/MainWindow.xaml.cs
@@ -40,27 +40,6 @@ namespace Codecool.Quest {
             }
         }
 
-        public void TestLaunch() {
-            BitmapImage src = new BitmapImage();
-            var img = @"pack://application:,,,/Resources/roguelikeDungeon_transparent.png";
-            src.BeginInit();
-            src.UriSource = new Uri(img);
-            src.CacheOption = BitmapCacheOption.OnLoad;
-            src.EndInit();
-
-            Image bodyImage = new Image {
-                Source = new CroppedBitmap(
-                    src,
-                    new Int32Rect(0, 0, 16, 16)),
-                Stretch = Stretch.None,
-                Width = 16,
-                Height = 16
-            };
-
-            gameCanvas.Children.Add(bodyImage);
-
-        }
-
         private void GameCanvas_KeyDown(object sender, KeyEventArgs e) {
             switch (e.Key) {
                 case Key.Left:
