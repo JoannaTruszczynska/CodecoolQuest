c6bf9ca157122f951bdb53b7917e433ff4cd2d9b
Bartlomiej Kwoka <bkwoka1@gmail.com>
2019-12-05 11:55:05 +0100

    Temporary commit

---
+++ CodecoolQuest/Models/Actors/Ghost.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Codecool.Quest.Models.Actors;
+
+namespace Codecool.Quest.Models.Things
+{
+    class Ghost: Skeleton
+    {
+        public Ghost(Cell cell) : base(cell)
+        {
+        }
+
+        public override string TileName { get; set; } = "ghost";
+
+        public override string Type { get; } = "skeleton";
+        public override int AttackStrength { get; set; } = 1;
+    }
+}
