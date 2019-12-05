c9b3126e002e98b8e90c2936a0c9264fd8fd2fd4
Bartlomiej Kwoka <bkwoka1@gmail.com>
2019-12-05 10:01:59 +0100

    Added, Extra map and MapPaths Class

---
+++ CodecoolQuest/MapsPaths.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Codecool.Quest
+{
+    public static class MapsPaths
+    {
+
+        public static string FirstLevel { get; } = "../../../Maps/firstLevel.txt";
+        public static string EndGame { get; } = "../../../Maps/EndGame.txt";
+
+    }
+    
+}
